871762805ddaaf9acf8738f52a1e3dd6df7daa6a
NikitaBhattacharya <55226362+NikitaBhattacharya@users.noreply.github.com>
2021-09-13 19:38:19 +0530

    Add files via upload

--- Learnings/StringAnagram.java
+++ Learnings/StringAnagram.java
@@ -1,14 +1,15 @@
 package com.amadeus.learnings;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class StringAnagram {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String a = "act";
-		String b = "tac";
-		boolean isAnagaram = StringAnagramSolution.isAnagram(a, b);
+		String a = "geeksforgeeks";
+		String b = "forgeeksgeeks";
+		boolean isAnagaram = StringAnagramSolution.isAnagramSol(a, b);
 		System.out.println(isAnagaram);
 	}
 
@@ -16,9 +17,25 @@ public class StringAnagram {
 
 
 class StringAnagramSolution {
+	
+	public static boolean isAnagramSol(String a,String b){
+		if(a.length() != b.length()) {
+			return false;
+		}
+		char[] string_a = a.toCharArray();
+		char[] string_b = b.toCharArray();
+		Arrays.sort(string_a);
+		Arrays.sort(string_b);
+		boolean status = Arrays.equals(string_a, string_b);  
+		return status;
+	}
+	
 	public static boolean isAnagram(String a,String b){
 		HashMap<Character, Integer> map_a = new HashMap<>();
 		HashMap<Character, Integer> map_b = new HashMap<>();
+		if(a.length() != b.length()) {
+			return false;
+		}
 		for(int i=0; i<a.length(); i++) {
 			char literal = a.charAt(i);
 			if(map_a.containsKey(literal)) {
